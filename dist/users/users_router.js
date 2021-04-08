5579f0a25ee10e08d9fb7633bb0009836d6b03a3
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-08 20:34:09 -0300

    create user model

--- dist/users/users_router.js
+++ dist/users/users_router.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersRouter = void 0;
 const router_1 = require("../common/router");
+const users_model_1 = require("./users_model");
 class UsersRouter extends router_1.Router {
     applyRoutes(application) {
         application.get('/users', (req, resp, next) => {
-            resp.json({ message: 'users router' });
+            users_model_1.User.findAll().then(users => {
+                resp.json(users);
+                return next();
+            });
         });
     }
 }
