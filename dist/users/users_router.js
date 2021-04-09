6cff42f099e16d70e27be6443e72771ffdfb4254
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-09 19:26:43 -0300

    merge delete parser

--- dist/users/users_router.js
+++ dist/users/users_router.js
@@ -48,12 +48,22 @@ class UsersRouter extends router_1.Router {
             users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
                 if (user) {
                     resp.json(user);
-                    return next();
                 }
                 else {
                     resp.send(404);
-                    return next();
                 }
+                return next();
+            });
+        });
+        application.del('/users/:id', (req, resp, next) => {
+            users_model_1.User.remove({ _id: req.params.id }).exec().then((cmdResult) => {
+                if (cmdResult.result.n) {
+                    resp.send(204);
+                }
+                else {
+                    resp.send(404);
+                }
+                return next();
             });
         });
     }
--- users/users_router.ts
+++ users/users_router.ts
@@ -51,11 +51,22 @@ class UsersRouter extends Router {
             User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
                 if (user) {
                     resp.json(user)
-                    return next()
                 } else {
                     resp.send(404)
-                    return next()
                 }
+                return next()
+
+            })
+        })
+
+        application.del('/users/:id', (req, resp, next) => {
+            User.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
+                if (cmdResult.result.n) {
+                    resp.send(204)
+                } else {
+                    resp.send(404)
+                }
+                return next()
             })
         })
     }
