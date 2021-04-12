e657f9b3523cbb2c11c8dda0c874b0963abf89cc
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-12 11:20:57 -0300

    filter by quering when get users using mongoose

--- users/users_model.ts
+++ users/users_model.ts
@@ -10,6 +10,9 @@ export interface User extends mongoose.Document {
     password: string
 }
 
+export interface UserModel extends mongoose.Model<User> {
+    findByEmail(email: string): Promise<User>
+}
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -45,6 +48,10 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByEmail = function (email: string) {
+    return this.findOne({ email })
+}
+
 const hashPassword = (obj, next) => {
     bcrypt.hash(obj.password, environment.security.saltRounds).then(hash => {
         obj.password = hash
@@ -71,4 +78,4 @@ userSchema.pre('save', saveMiddleware)
 userSchema.pre('findOneAndUpdate', updateMiddleware)
 userSchema.pre('update', updateMiddleware)
 
-export const User = mongoose.model<User>('User', userSchema)
+export const User = mongoose.model<User, UserModel>('User', userSchema)
