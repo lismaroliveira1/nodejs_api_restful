163b326519baf642980dcf80c8e910f4933c7027
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-12 14:11:57 -0300

    should call /users url and return 200 status code response

--- dist/users/users.test.js
+++ dist/users/users.test.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jest");
-test("first test", () => {
+const request = require("supertest");
+test("call /users and return 200 status code", () => {
+    request('http://localhost:3000').get('/users').then(response => {
+        expect(response.status).toBe(200);
+    }).catch(fail);
 });
--- users/users.test.ts
+++ users/users.test.ts
@@ -1,5 +1,8 @@
 import 'jest'
+import * as request from 'supertest'
 
-test("first test", () => {
-
+test("call /users and return 200 status code", () => {
+  request('http://localhost:3000').get('/users').then(response => {
+    expect(response.status).toBe(200)
+  }).catch(fail)
 })
