17a3f954b96cb9c1cc8eb03e64ca9166740ddbff
lismaroliveira1 <englismaroliveira@gmail.com>
2021-04-14 07:32:01 -0300

    refactor message response

--- common/router.ts
+++ common/router.ts
@@ -21,7 +21,7 @@ export abstract class Router extends EventEmitter {
             else {
                 throw new NotFoundError('Document not found')
             }
-            return next()
+            return next(false)
         }
     }
 
